66a323d39e887d5c8f1d12adbd55c775b0b9d130
chandan gupta <94867034+chandan-devs@users.noreply.github.com>
2024-08-04 12:56:12 +0000

    project setup using NextJS

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Music Academy",
+  description:
+    "music academy website for learning music online and offline with professional teachers and musicians from around the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <h1 className="text-center text-3xl">Music Academy</h1>
+    </div>
+  );
+}
